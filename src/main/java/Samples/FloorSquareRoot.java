50a476b117ac44aa9a27c3adaca164029f754ebf
dasoumao <msx@live.ca>
2025-03-25 17:00:51 +0800

    change start

--- src/main/java/Samples/FloorSquareRoot.java
+++ src/main/java/Samples/FloorSquareRoot.java
@@ -4,12 +4,12 @@ public class FloorSquareRoot {
     public static int floorSqrt(int x) {
         if (x == 0 || x == 1) return x;
 
-        int start = 2, end = x, ans = 0;
+        int start = 1, end = x, ans = 0;
         while (start <= end) {
             int mid = (start + end) / 2;
             if (mid * mid == x) return mid;
             if (mid * mid < x) {
-                start = mid + 1;
+                start = mid + 10;
                 ans = mid;
             } else {
                 end = mid - 1;
